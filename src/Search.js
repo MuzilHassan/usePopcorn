51c084ba14c52bcac1014940902dd7cb4171b3f4
Muziljohn <muzilhassan9@gmail.com>
2024-03-11 00:10:36 +0500

    used useRef to handle keydonw events

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,27 @@
+import { useEffect, useRef } from "react";
+
 export function Search({ query, setQuery }) {
+  const searchRef = useRef();
+
+  useEffect(() => {
+    function func(e) {
+      if (document.activeElement === searchRef.current) return;
+      console.log(e.code);
+      if (e.code == "Enter") {
+        searchRef.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", func);
+    return () => document.removeEventListener("keydown", func);
+  }, []);
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
+      ref={searchRef}
       onChange={(e) => setQuery(e.target.value)}
     />
   );
